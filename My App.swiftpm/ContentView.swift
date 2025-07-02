06258464517dfa466a89f2522dc60fe1430d4f14
G.J.Hilton <gjhilton@users.noreply.github.com>
2025-07-02 08:49:36 +0100

    anima

--- My App.swiftpm/ContentView.swift
+++ My App.swiftpm/ContentView.swift
@@ -1,21 +1,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var offsetX: CGFloat = -150 // start off left
+    
     var body: some View {
         ZStack {
             Color.white.edgesIgnoringSafeArea(.all)
             
+            Rectangle()
+                .fill(Color.black)
+                .frame(width: 250, height: 100)
+                .position(x: 200, y: 200)
+            
             Text("Blend Mode Text")
                 .font(.largeTitle)
                 .fontWeight(.bold)
-                .foregroundColor(.black)
-                .position(x: 200, y: 200)
-            Rectangle()
-                .fill(Color.white)
-                .frame(width: 250, height: 100)
-                .position(x: 200, y: 200)
+                .foregroundColor(.white)
                 .blendMode(.difference)
-            
+                .position(x: 200 + offsetX, y: 200)
+                .onAppear {
+                    let distance: CGFloat = 300 // total horizontal distance to move
+                    offsetX = -distance / 2
+                    withAnimation(
+                        Animation.linear(duration: 3)
+                            .repeatForever(autoreverses: true)
+                    ) {
+                        offsetX = distance / 2
+                    }
+                }
         }
     }
 }
