a9b2a5e8ea0973b61c4a33087a88be086e0062a3
G.J.Hilton <gjhilton@users.noreply.github.com>
2025-07-12 14:54:57 +0100

    p

--- legacy/ProofOfConcept.swiftpm/Shared/HelpViewController+SwiftUI.swift
+++
@@ -1,9 +0,0 @@
-import SwiftUI
-import UIKit
-
-struct HelpViewControllerWrapper: UIViewControllerRepresentable {
-    func makeUIViewController(context: Context) -> HelpViewController {
-        return HelpViewController()
-    }
-    func updateUIViewController(_ uiViewController: HelpViewController, context: Context) {}
-} 
